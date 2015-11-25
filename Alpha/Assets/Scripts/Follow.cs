69bceca9858f95fa6a254392744f2a01ce17d20a
kpcorda <kpcorda@gmail.com>
2015-11-24 18:16:10 -0600

    commit

---
+++ Alpha/Assets/Scripts/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+     public Transform target;
+    public Vector3 distance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + distance;
+    }
+}
---
+++ Alpha/Assets/Scripts/TriggerMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerMenu : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel("VisMenu");
+        }
+
+    }
+
+}
