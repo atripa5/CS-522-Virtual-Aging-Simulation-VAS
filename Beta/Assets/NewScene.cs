3ea4bb0cb7949c954f57cc6c8959ea1d82e5be59
kpcorda <kpcorda@gmail.com>
2015-12-05 08:02:44 -0600

    commit

---
+++ Beta/Assets/NewScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScene : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel("Information2");
+        }
+
+    }
+
+}
---
+++ Beta/Assets/Scripts/walking.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walking : MonoBehaviour {
+
+    private Animator animator;
+    private float vertical;
+    private float horizontal;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        vertical = Input.GetAxis ("Vertical");
+        horizontal = Input.GetAxis ("Horizontal");
+        animator.SetFloat ("walk", vertical);
+        animator.SetFloat ("turn", horizontal);
+	
+	}
+}
